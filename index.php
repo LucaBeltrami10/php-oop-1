f257e7a750c2eae99b9bad74947c1c57959da1d7
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-25 22:15:30 +0100

    inserito card con ciclo foreach

--- index.php
+++ index.php
@@ -80,6 +80,27 @@ $movies = [$ilPadrino, $titanic, $matrix, $laLaLand];
 
 <body>
 
+    <div id="app">
+        <main class="container">
+            <div class="row">
+                <?php foreach ($movies as $movie) { ?>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </main>
+    </div>
+
+
 </body>
 
 </html>
